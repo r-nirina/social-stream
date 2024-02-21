835ba147ae33f3645106212e2544812d4107b466
Randy <randy@andriamaro.com>
2024-02-21 08:29:01 +0100

    ✨ Generated chart view component

---
+++ src/app/routes/chart/chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chart-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart.component.html',
+  styleUrl: './chart.component.scss'
+})
+export class ChartComponent {
+
+}
