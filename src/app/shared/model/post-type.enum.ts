372f95958be217dda2cb9eeeb1c806980adb1fcf
Randy <randy@andriamaro.com>
2024-02-22 00:00:33 +0100

    🏷️ Added post type enum

---
+++ src/app/shared/model/post-type.enum.ts
@@ -0,0 +1,8 @@
+export enum PostType {
+  Pin = 'pin',
+  InstagramMedia = 'instagram_media',
+  YouTubeVideo = 'youtube_video',
+  Article = 'article',
+  Tweet = 'tweet',
+  FacebookStatus = 'facebook_status',
+}
