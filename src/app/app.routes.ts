7002635ff4fb62cfb2f9089f72a611038deb64d7
Randy <randy@andriamaro.com>
2024-02-21 17:37:23 +0100

    ✨ Added routing to chart view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ChartComponent } from "./routes/chart/chart.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'chart', component: ChartComponent },
+];
