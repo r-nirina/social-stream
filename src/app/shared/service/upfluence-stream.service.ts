b69eb5a1e961bd91e4c66ce4c493aa9ab4901bd3
Randy <randy@andriamaro.com>
2024-02-24 17:37:50 +0100

    ✨ Generated UpfluenceStream web worker

--- src/app/shared/service/upfluence-stream.service.ts
+++ src/app/shared/service/upfluence-stream.service.ts
@@ -8,3 +8,15 @@ export class UpfluenceStreamService {
     // TODO
   }
 }
+
+if (typeof Worker !== 'undefined') {
+  // Create a new
+  const worker = new Worker(new URL('./upfluence-stream.worker', import.meta.url));
+  worker.onmessage = ({ data }) => {
+    console.log(`page got message: ${data}`);
+  };
+  worker.postMessage('hello');
+} else {
+  // Web Workers are not supported in this environment.
+  // You should add a fallback so that your program still executes correctly.
+}
